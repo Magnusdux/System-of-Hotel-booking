1fce559518fea2a765b257f0655a23d186469e8b
Magnusdux <pc.player.no.one@gmail.com>
2024-03-19 12:01:40 +0200

    Add Copy Constructor, id for booking, method getBookinginfo, makeBooking, getBookingQuantity,ostream and istream operators

--- Booking.cpp
+++ Booking.cpp
@@ -3,9 +3,20 @@
 //
 
 #include "Booking.h"
+#include <iostream>
+using namespace std;
 
 Booking::Booking(int rent, int number, int price)
         : dayOfRent{rent}, numberOfroom{number}, priceOfRent{price} {}
+
+Booking::Booking(const Booking& b)
+{
+    cout << "Copy constructor called " << endl;
+    this->dayOfRent = b.dayOfRent;
+    this->numberOfroom = b.numberOfroom;
+    this->priceOfRent = b.priceOfRent;
+
+}
 
 int Booking::getDayOfRent() const {
     return dayOfRent;
@@ -30,7 +41,35 @@ int Booking::getPriceOfRent() const {
 void Booking::setPriceOfRent(int priceOfRent) {
     Booking::priceOfRent = priceOfRent;
 }
+void Booking::makeBooking()
+{
+    Booking::isBooked = true;
 
+    Booking::bookingQuantity++;
+}
+
+void Booking::getBookingInfo() {
+    cout << "Booking status: " << (isBooked ? "successfully booked" : "pending booking") << endl;
+    cout << "Your booking details:"  << "\n";
+    cout << "Booking id: " << id << "\n";
+    cout << "days of rent: " << dayOfRent << "\n";
+    cout << "number of rooms: " << numberOfroom  << "\n";
+    cout << "price of Rent: " << priceOfRent  << "\n";
+}
+
+void Booking::getBookingQuantity() {
+    cout << "The Quantity of booking:" << bookingQuantity;
+}
 Booking::Booking(int number, int price) : Booking{1, number, price} {
 
+}
+
+ostream &operator<<(ostream &os, const Booking &booking) {
+    os << "id: " << booking.id << " dayOfRent: " << booking.dayOfRent << " numberOfroom: " << booking.numberOfroom
+       << " priceOfRent: " << booking.priceOfRent << " isBooked: " << booking.isBooked;
+    return os;
+}
+istream &operator>>(istream &is, Booking &booking) {
+    is >> booking.priceOfRent;
+    return is;
 }
--- Booking.h
+++ Booking.h
@@ -4,16 +4,24 @@
 
 #ifndef UNTITLED_BOOKING_H
 #define UNTITLED_BOOKING_H
-
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 class Booking {
+    const int id = rand() % 100 + 1;
     int dayOfRent;
     int numberOfroom;
     int priceOfRent;
+    bool isBooked = false;
+    static int bookingQuantity;
 public:
+    Booking() {};
     Booking(int number, int price);
 
     Booking (int rent, int number, int price );
+
+    Booking(const Booking& b);
 
     int getDayOfRent() const;
 
@@ -26,6 +34,16 @@ public:
     int getPriceOfRent() const;
 
     void setPriceOfRent(int priceOfRent);
+
+    void makeBooking();
+
+    void getBookingInfo();
+
+    static void getBookingQuantity();
+
+    friend ostream &operator<<(ostream &os, const Booking &booking);
+
+    friend istream &operator>>(istream &is, Booking &booking);
 
 
 };
